b9de610126b3a7033a999033803b49e4b02ce936
pkellner <peter@peterkellner.net>
2022-01-22 17:56:05 -0800

    wip

--- pages/api/cities.js
+++ pages/api/cities.js
@@ -1,6 +1,8 @@
-import citiesData from "../../data/cities.json";
-
 export default async function handler(req, res) {
+  function getCitiesData() {
+    return citiesData;
+  }
+
   function runDelayMiddleware(req, res) {
     return new Promise((resolve) => {
       setTimeout(resolve, 1000);
@@ -14,7 +16,7 @@ export default async function handler(req, res) {
       count = parseInt(req.query.displayCount);
     }
 
-    const resultData = citiesData
+    const resultData = getCitiesData()
       .map(({ id, city, state, population }) => {
         return {
           id,
@@ -32,6 +34,69 @@ export default async function handler(req, res) {
         return 0;
       });
 
-    res.status(200).json(JSON.stringify(resultData, null, "\t"));
+    res.status(200).json(JSON.stringify(resultData, null, '\t'));
   }
 }
+
+const citiesData = [
+  {
+    id: 4,
+    city: 'New York',
+    state: 'New York',
+    population: '8405837',
+  },
+  {
+    id: 5,
+    city: 'Los Angeles',
+    state: 'California',
+    population: '3884307',
+  },
+  {
+    id: 6,
+    city: 'Chicago',
+    state: 'Illinois',
+    population: '2718782',
+  },
+  {
+    id: 7,
+    city: 'Houston',
+    state: 'Texas',
+    population: '2195914',
+  },
+  {
+    id: 8,
+    city: 'Philadelphia',
+    state: 'Pennsylvania',
+    population: '1553165',
+  },
+  {
+    id: 9,
+    city: 'Phoenix',
+    state: 'Arizona',
+    population: '1513367',
+  },
+  {
+    id: 1,
+    city: 'San Antonio',
+    state: 'Texas',
+    population: '1409019',
+  },
+  {
+    id: 10,
+    city: 'San Diego',
+    state: 'California',
+    population: '1355896',
+  },
+  {
+    id: 11,
+    city: 'Dallas',
+    state: 'Texas',
+    population: '1257676',
+  },
+  {
+    id: 3,
+    city: 'San Francisco',
+    state: 'California',
+    population: '837442',
+  },
+];
--- pages/api/citydetail.js
+++ pages/api/citydetail.js
@@ -1,6 +1,3 @@
-import cityDetails from "../../data/cityDetails.json";
-//import detail from "../../data/historicalData.json";
-
 export default async function handler(req, res) {
   function runDelayMiddleware(req, res) {
     return new Promise((resolve) => {
@@ -15,36 +12,119 @@ export default async function handler(req, res) {
       cityId = parseInt(req.query.cityId);
     }
 
-    // const xx = detail.map((rec) => {
-    //   return {
-    //     id: rec.id,
-    //     city: rec.city,
-    //     state: rec.state,
-    //     population: rec.population,
-    //     temperature: rec.sensors[0].temperature,
-    //     airQualityPPM: rec.sensors[0].pm25,
-    //   };
-    // });
-    // const str = JSON.stringify(xx);
-    // console.log(str);
-    
-    const resultData = cityDetails
+    function getCityDetails() {
+      return cityDetails;
+    }
+
+    const resultData = getCityDetails()
       .filter((rec) => rec.id == cityId)
-      .map(({ id, city, state, population, temperature, airQualityPPM }) => {
-        return {
+      .map(
+        ({
           id,
           city,
           state,
           population,
           temperature,
           airQualityPPM,
-        };
-      });
+        }) => {
+          return {
+            id,
+            city,
+            state,
+            population,
+            temperature,
+            airQualityPPM,
+          };
+        },
+      );
 
     if (resultData.length > 0) {
-      res.status(200).json(JSON.stringify(resultData[0], null, "\t"));
+      res.status(200).json(JSON.stringify(resultData[0], null, '\t'));
     } else {
-      res.status(404).send("record not found");
+      res.status(404).send('record not found');
     }
   }
 }
+
+const cityDetails = [
+  {
+    id: 4,
+    city: 'New York',
+    state: 'New York',
+    population: '8405837',
+    temperature: 43,
+    airQualityPPM: 24,
+  },
+  {
+    id: 5,
+    city: 'Los Angeles',
+    state: 'California',
+    population: '3884307',
+    temperature: 58,
+    airQualityPPM: 63,
+  },
+  {
+    id: 6,
+    city: 'Chicago',
+    state: 'Illinois',
+    population: '2718782',
+    temperature: 58,
+    airQualityPPM: 60,
+  },
+  {
+    id: 7,
+    city: 'Houston',
+    state: 'Texas',
+    population: '2195914',
+    temperature: 80,
+    airQualityPPM: 3,
+  },
+  {
+    id: 8,
+    city: 'Philadelphia',
+    state: 'Pennsylvania',
+    population: '1553165',
+    temperature: 43,
+    airQualityPPM: 18,
+  },
+  {
+    id: 9,
+    city: 'Phoenix',
+    state: 'Arizona',
+    population: '1513367',
+    temperature: 79,
+    airQualityPPM: 63,
+  },
+  {
+    id: 1,
+    city: 'San Antonio',
+    state: 'Texas',
+    population: '1409019',
+    temperature: 82,
+    airQualityPPM: 36,
+  },
+  {
+    id: 10,
+    city: 'San Diego',
+    state: 'California',
+    population: '1355896',
+    temperature: 67,
+    airQualityPPM: 69,
+  },
+  {
+    id: 11,
+    city: 'Dallas',
+    state: 'Texas',
+    population: '1257676',
+    temperature: 77,
+    airQualityPPM: 30,
+  },
+  {
+    id: 12,
+    city: 'San Jose',
+    state: 'California',
+    population: '998537',
+    temperature: 80,
+    airQualityPPM: 3,
+  },
+];
